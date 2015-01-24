91698a1a39b8b210435702f3d284f7008d43d9e9
jude <jude@affle.com>
2015-01-24 18:03:07 +0800

    initial commit

---
+++ JavaClassObjects/src/Application.java
@@ -0,0 +1,15 @@
+public class Application
+    {
+    public static void main(String[] args)
+        {
+        Person person1 = new Person();
+        person1.name = "Joe Bloggins";
+        person1.age = 37;
+
+        Person person2 = new Person();
+        person2.name = "sarah smith";
+        person2.age = 20;
+
+            System.out.println(person1.name);
+        }
+    }
